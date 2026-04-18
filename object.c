62577f95c349c9e7dc30ca80055b555f077aaf77
almondcrossaint <ekadantaele@rediffmail.com>
2026-04-18 18:54:02 +0000

    phase 1: implemented object_read with parsing and integrity check

--- object.c
+++ object.c
@@ -236,5 +236,74 @@ int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_
     (void)type_out;
     (void)data_out;
     (void)len_out;
-    return -1;
+    char path[512];
+    object_path(id, path, sizeof(path));
+
+    FILE *f = fopen(path, "rb");
+    if (!f)
+        return -1;
+
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    rewind(f);
+
+    uint8_t *buffer = malloc(size);
+    if (!buffer)
+    {
+        fclose(f);
+        return -1;
+    }
+
+    if (fread(buffer, 1, size, f) != (size_t)size)
+    {
+        free(buffer);
+        fclose(f);
+        return -1;
+    }
+    fclose(f);
+
+    // verify hash
+    ObjectID check;
+    compute_hash(buffer, size, &check);
+
+    if (memcmp(check.hash, id->hash, HASH_SIZE) != 0)
+    {
+        free(buffer);
+        return -1;
+    }
+
+    // parse header
+    char *null_pos = memchr(buffer, '\0', size);
+    if (!null_pos)
+    {
+        free(buffer);
+        return -1;
+    }
+
+    size_t header_len = null_pos - (char *)buffer + 1;
+
+    // determine type
+    if (strncmp((char *)buffer, "blob", 4) == 0)
+        *type_out = OBJ_BLOB;
+    else if (strncmp((char *)buffer, "tree", 4) == 0)
+        *type_out = OBJ_TREE;
+    else if (strncmp((char *)buffer, "commit", 6) == 0)
+        *type_out = OBJ_COMMIT;
+    else
+    {
+        free(buffer);
+        return -1;
+    }
+
+    // extract data
+    size_t data_len = size - header_len;
+    void *data = malloc(data_len);
+
+    memcpy(data, buffer + header_len, data_len);
+
+    *data_out = data;
+    *len_out = data_len;
+
+    free(buffer);
+    return 0;
 }
